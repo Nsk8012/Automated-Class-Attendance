a4bb4c486fe61fdb55cda1e01d9722e6a271452f
Nishanth S <nishanth818@gmail.com>
2020-11-24 11:07:04 +0530

    Working good, need to add TIME

--- webOpener.py
+++ webOpener.py
@@ -1,8 +1,6 @@
-import os
+from time import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -16,19 +14,21 @@ emailBox.send_keys('nishanth818@gmail.com')
 passWord.send_keys('Neya@8012')
 enterButton.click()
 
+tripleButton = driver.find_element_by_xpath(
+    '//*[@id="root"]/header/div/div/button')
+tripleButton.click()
+programBox = driver.find_element_by_xpath(
+    '/html/body/div[2]/div[3]/div/div[2]/div/div/div')
+programBox.click()
+sec_B = driver.find_element_by_xpath('//*[@id="menu-"]/div[3]/ul/li[1]')
+sec_B.click()
 
-try:
-    tripleButton = driver.find_element_by_xpath(
-        '//*[@id="root"]/header/div/div/button')
-    tripleButton.click()
-    programBox = driver.find_element_by_xpath(
-        '/html/body/div[2]/div[3]/div/div[2]/div/div/div')
-    programBox.click()
-    sec_B = driver.find_element_by_xpath('//*[@id="menu-"]/div[3]/ul/li[1]')
-    sec_B.click()
+driver.implicitly_wait(5)
+print("waiting")
+switchBox = driver.find_element_by_xpath(
+    '/html/body/div[3]/div[3]/div/div[3]/div/button[2]')
+switchBox.click()
 
-    switchButton = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.name, 'Switch Program')))
-
-except:
-    print('Not opening')
+upcomingBox = driver.find_element_by_xpath(
+    '//*[@id="main"]/div/div/div/div[3]/div/div[1]/div[2]/div/button[1]')
+upcomingBox.click()
